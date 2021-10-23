aa68846fc5eb1897e142ad9c5281a09d598c1029
Michael Fu Kai Ting <michaelfukai.ting@student.adelaide.edu.au>
2021-10-23 17:00:36 +1030

    set up scss

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './nav.scss'
+
+const Navbar = () => {
+    return (
+        <div className={'check'}>
+            <h1>heeelo</h1>
+        </div>
+    );
+};
+
+export default Navbar;
